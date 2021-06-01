dac1b15af0c071f6f8befc5ee20461b446d69cf0
harini <harinisiva1997@gmail.com>
2021-06-01 23:38:58 +0530

    reading and dispatching from new slice

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Counter from './components/Counter';
 import Header from './components/Header'
 import Auth from './components/Auth'
+import { useSelector } from 'react-redux';
+import UserProfile from './components/UserProfile';
 
 function App() {
+  const isAuth = useSelector(state => state.auth.isAuthenticated)
   return (
     <React.Fragment>
       <Header />
-      <Auth />
+      {isAuth ? < UserProfile /> : <Auth />}
       <Counter />
     </React.Fragment>
   );
